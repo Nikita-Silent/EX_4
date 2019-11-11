cc9d119210fbe283ad24ba6437a9f977d8e5002c
nikita-silent <npcweb@mail.ru>
2019-11-11 11:16:31 +0700

    Add " " in enter data

--- local.py
+++ local.py
@@ -9,4 +9,5 @@ TXT_0 = 'Choose option (encrypt / decrypt): '
 TXT_1 = 'Enter the data: '
 AZBUKA_EN = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 NUM = 26
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if loc == lc.EN:
     fill = input(lc.TXT_1).lower()
     if ans == lc.ANS_1:
         for i in range(len(fill)):
+            if fill[i] == ' ':
+                s.append(' ')
             for j in range(lc.NUM):
                 try:
                     if fill[i] == lc.AZBUKA_EN[j]:
@@ -18,6 +20,8 @@ if loc == lc.EN:
         print(''.join(s))
     elif ans == lc.ANS_2:
         for i in range(len(fill)):
+            if fill[i] == ' ':
+                s.append(' ')
             for j in range(lc.NUM):
                 if fill[i] == lc.AZBUKA_EN[j]:
                     s.append(lc.AZBUKA_EN[j - 1])
@@ -31,6 +35,8 @@ elif loc == lc.RU:
     fill = input(lr.TX_1).lower()
     if ans == lr.AN_1:
         for i in range(len(fill)):
+            if fill[i] == ' ':
+                s.append(' ')
             for j in range(lr.NUM_1):
                 try:
                     if fill[i] == lr.AZBUKA_RU[j]:
@@ -41,6 +47,8 @@ elif loc == lc.RU:
         print(''.join(s))
     elif ans == lr.AN_2:
         for i in range(len(fill)):
+            if fill[i] == ' ':
+                s.append(' ')
             for j in range(lr.NUM_1):
                 if fill[i] == lr.AZBUKA_RU[j]:
                     s.append(lr.AZBUKA_RU[j - 1])
